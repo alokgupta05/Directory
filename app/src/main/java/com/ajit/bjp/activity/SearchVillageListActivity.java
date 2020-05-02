8f4cb99745d152dc753fc693a254a34997f830aa
kanishk <kanishk36@gmail.com>
2020-05-02 19:02:41 +0400

    changes for sharing village details

--- app/src/main/java/com/ajit/bjp/activity/SearchVillageListActivity.java
+++ app/src/main/java/com/ajit/bjp/activity/SearchVillageListActivity.java
@@ -5,29 +5,22 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DefaultItemAnimator;
-import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.SearchView;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
 import com.ajit.bjp.R;
-import com.ajit.bjp.adapter.PersonListAdapter;
 import com.ajit.bjp.adapter.VillageListAdapter;
-import com.ajit.bjp.model.Entry;
-import com.ajit.bjp.model.Example;
-import com.ajit.bjp.model.Person;
-import com.ajit.bjp.model.PersonFeed;
 import com.ajit.bjp.model.Village;
 import com.ajit.bjp.model.VillageEntry;
 import com.ajit.bjp.util.AppCache;
+import com.ajit.bjp.util.AppUtils;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import static com.ajit.bjp.activity.SearchActivity.PERSON_KEY;
-import static com.ajit.bjp.activity.SearchActivity.RESPONSE_DATA;
 import static com.ajit.bjp.activity.VillageActivity.VILLAGE_KEY;
 
 public class SearchVillageListActivity extends AppCompatActivity implements VillageListAdapter.OnClickPerson {
@@ -35,6 +28,8 @@ public class SearchVillageListActivity extends AppCompatActivity implements Vill
     private RecyclerView listViewPersonName;
     private SearchView mSearchView;
     private VillageListAdapter personListAdapter;
+    private Menu mMenu;
+    private List<VillageEntry> villageList;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -42,13 +37,14 @@ public class SearchVillageListActivity extends AppCompatActivity implements Vill
 
         setContentView(R.layout.layout_search_name);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        setData();
         listViewPersonName = findViewById(R.id.list_view_name);
-         personListAdapter = new VillageListAdapter(setData());
+        personListAdapter = new VillageListAdapter(villageList);
         personListAdapter.setOnClickPerson(this);
         listViewPersonName.setItemAnimator(new DefaultItemAnimator());
         listViewPersonName.setLayoutManager(new LinearLayoutManager(this));
         listViewPersonName.setAdapter(personListAdapter);
-        listViewPersonName.addItemDecoration(new DividerItemDecoration(listViewPersonName.getContext(), DividerItemDecoration.VERTICAL));
+//        listViewPersonName.addItemDecoration(new DividerItemDecoration(listViewPersonName.getContext(), DividerItemDecoration.VERTICAL));
         mSearchView = findViewById(R.id.search_view);
        // mSearchView.setOnClickListener(this);
         mSearchView.clearFocus();
@@ -66,14 +62,65 @@ public class SearchVillageListActivity extends AppCompatActivity implements Vill
 
     }
 
-
-    private List<VillageEntry> setData(){
-        List<VillageEntry> villageList = (List<VillageEntry>) AppCache.INSTANCE.getValueOfAppCache("VILLAGE_LIST");
-        return villageList;
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_karyakarta, menu);
+        mMenu = menu;
+        menu.findItem(R.id.menuSearch).setVisible(false);
+        menu.findItem(R.id.menuShare).setVisible(false);
+        menu.findItem(R.id.menuClose).setVisible(false);
+        return true;
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
 
+            case android.R.id.home:
+                onBackPressed();
+                return true;
 
+            case R.id.menuCheckBox:
+                if(villageList != null) {
+                    mMenu.findItem(R.id.menuSearch).setVisible(false);
+                    mMenu.findItem(R.id.menuCheckBox).setVisible(false);
+                    mMenu.findItem(R.id.menuShare).setVisible(true);
+                    mMenu.findItem(R.id.menuClose).setVisible(true);
+
+                    personListAdapter.isMultipleSharing(true);
+                }
+                return true;
+
+            case R.id.menuShare:
+                List<VillageEntry> sharingList = personListAdapter.getSharingList();
+                if(!sharingList.isEmpty()) {
+                    shareMultipleContacts(sharingList);
+
+                    mMenu.findItem(R.id.menuSearch).setVisible(true);
+                    mMenu.findItem(R.id.menuCheckBox).setVisible(true);
+                    mMenu.findItem(R.id.menuShare).setVisible(false);
+                    mMenu.findItem(R.id.menuClose).setVisible(false);
+
+                    personListAdapter.isMultipleSharing(false);
+                }
+                return true;
+
+            case R.id.menuClose:
+                mMenu.findItem(R.id.menuSearch).setVisible(true);
+                mMenu.findItem(R.id.menuCheckBox).setVisible(true);
+                mMenu.findItem(R.id.menuShare).setVisible(false);
+                mMenu.findItem(R.id.menuClose).setVisible(false);
+
+                personListAdapter.isMultipleSharing(false);
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void setData(){
+        villageList = (List<VillageEntry>) AppCache.INSTANCE.getValueOfAppCache("VILLAGE_LIST");
+    }
 
     private Village getVillageFromEntry(VillageEntry entry){
 
@@ -92,19 +139,28 @@ public class SearchVillageListActivity extends AppCompatActivity implements Vill
 
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()){
-            case android.R.id.home:
-                onBackPressed();
-                return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
     public void onPersonClicked(VillageEntry person) {
         Intent intentPerson = new Intent(this, VillageDetailActivity.class);
         intentPerson.putExtra(VILLAGE_KEY, getVillageFromEntry(person));
         startActivity(intentPerson);
+    }
+
+    @Override
+    public void onVillageDetailsShareClick(String details) {
+        AppUtils.shareContent(SearchVillageListActivity.this, details);
+    }
+
+    private void shareMultipleContacts(List<VillageEntry> sharingList) {
+        String content = "";
+        for (int i = 0; i < sharingList.size(); i++) {
+            VillageEntry villageEntry = sharingList.get(i);
+            content = content.concat(Integer.toString(i + 1)).concat(")\n")
+                    .concat("Details: ").concat(villageEntry.getGsx$details().get$t()).concat("\n")
+                    .concat("Scheme: ").concat(villageEntry.getGsx$scheme().get$t()).concat("\n")
+                    .concat("Status: ").concat(villageEntry.getGsx$status().get$t()).concat("\n")
+                    .concat("Sanctioned Amount :").concat(villageEntry.getGsx$sanctionedamount().get$t()).concat("\n\n");
+        }
+
+        AppUtils.shareContent(this, content);
     }
 }
--- app/src/main/java/com/ajit/bjp/adapter/VillageListAdapter.java
+++ app/src/main/java/com/ajit/bjp/adapter/VillageListAdapter.java
@@ -2,17 +2,16 @@ package com.ajit.bjp.adapter;
 
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
-import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Filter;
-import android.widget.Filterable;
+import android.widget.CheckBox;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import com.ajit.bjp.R;
-import com.ajit.bjp.model.Person;
 import com.ajit.bjp.model.VillageEntry;
+import com.ajit.bjp.util.AppConstants;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -25,9 +24,12 @@ public class VillageListAdapter extends RecyclerView.Adapter<RecyclerView.ViewHo
 
     private List<VillageEntry> villageEntryList;
     private OnClickPerson onClickPerson;
+    private boolean mIsMultipleSharing;
+    private List<VillageEntry> mSharingList;
 
     public interface OnClickPerson{
         void onPersonClicked(VillageEntry person);
+        void onVillageDetailsShareClick(String details);
     }
 
     public void setOnClickPerson(OnClickPerson onClickPerson){
@@ -36,16 +38,18 @@ public class VillageListAdapter extends RecyclerView.Adapter<RecyclerView.ViewHo
 
     public VillageListAdapter(List<VillageEntry> personList) {
         this.villageEntryList = personList;
+        if(villageEntryList == null) {
+            villageEntryList = new ArrayList<>();
+        }
+        mSharingList = new ArrayList<>();
     }
-
-
 
     @NonNull
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
 
         View itemView = LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.row_person_information, parent, false);
+                .inflate(R.layout.row_village, parent, false);
         return new PersonHolderView(itemView);
 
     }
@@ -53,7 +57,50 @@ public class VillageListAdapter extends RecyclerView.Adapter<RecyclerView.ViewHo
     @Override
     public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
         final VillageEntry personInfo = villageEntryList.get(position);
+
+        String srNo = Integer.toString(position+1).concat("/").concat(Integer.toString(villageEntryList.size()));
+
         PersonHolderView branchHolderView = (PersonHolderView) holder;
+
+        branchHolderView.lblSrNo.setText(AppConstants.SR_NO.concat(srNo));
+        branchHolderView.txtDetails.setText(personInfo.getGsx$details().get$t());
+        branchHolderView.txtScheme.setText(personInfo.getGsx$scheme().get$t());
+        branchHolderView.txtStatus.setText(personInfo.getGsx$status().get$t());
+        branchHolderView.txtSanctionedAmount.setText(personInfo.getGsx$sanctionedamount().get$t());
+
+        if(mIsMultipleSharing) {
+            branchHolderView.checkbox.setVisibility(View.VISIBLE);
+        } else {
+            branchHolderView.checkbox.setChecked(false);
+            branchHolderView.checkbox.setVisibility(View.GONE);
+        }
+
+        if(mSharingList.contains(personInfo)) {
+            branchHolderView.checkbox.setChecked(true);
+        } else {
+            branchHolderView.checkbox.setChecked(false);
+        }
+
+        branchHolderView.checkbox.setOnClickListener( view -> {
+            if(branchHolderView.checkbox.isChecked()) {
+                mSharingList.add(personInfo);
+            } else {
+                mSharingList.remove(personInfo);
+            }
+        });
+
+        branchHolderView.btnShare.setOnClickListener(
+                (View v) -> {
+                    if(onClickPerson != null && !mIsMultipleSharing) {
+                        String content = "Details: "+ personInfo.getGsx$details().get$t() +
+                                "\nScheme: "+ personInfo.getGsx$scheme().get$t() +
+                                "\nStatus:" + personInfo.getGsx$status().get$t() +
+                                "\nSanctioned Amount:" + personInfo.getGsx$sanctionedamount().get$t();
+                        onClickPerson.onVillageDetailsShareClick(content);
+                    }
+                }
+        );
+
         branchHolderView.itemView.setOnClickListener(
                 (View v) -> {
                     if (onClickPerson != null) {
@@ -61,27 +108,40 @@ public class VillageListAdapter extends RecyclerView.Adapter<RecyclerView.ViewHo
                     }
                 }
         );
-        branchHolderView.name.setText("Details: "+ personInfo.getGsx$details().get$t());
-        branchHolderView.department.setText("Scheme: "+personInfo.getGsx$scheme().get$t() + "\nStatus: " + personInfo.getGsx$status().get$t());
-
     }
 
     @Override
     public int getItemCount() {
-        return villageEntryList != null ? villageEntryList.size() : 0;
+        return villageEntryList.size();
     }
 
+    public void isMultipleSharing(boolean isMultipleSharing) {
+        mIsMultipleSharing = isMultipleSharing;
+        if(!isMultipleSharing) {
+            mSharingList.clear();
+        }
+        notifyDataSetChanged();
+    }
+
+    public List<VillageEntry> getSharingList() {
+        return mSharingList;
+    }
 
     static class PersonHolderView extends RecyclerView.ViewHolder {
-        private TextView name;
-        private TextView department;
-
+        TextView lblSrNo, txtDetails, txtScheme, txtStatus, txtSanctionedAmount;
+        ImageButton btnShare;
+        CheckBox checkbox;
 
         PersonHolderView(View view) {
             super(view);
-            this.name = view.findViewById(R.id.text_name);
-            this.department = view.findViewById(R.id.text_depart);
+            lblSrNo = view.findViewById(R.id.lblSrNo);
+            txtDetails = view.findViewById(R.id.txtDetails);
+            txtScheme = view.findViewById(R.id.txtScheme);
+            txtStatus = view.findViewById(R.id.txtStatus);
+            txtSanctionedAmount = view.findViewById(R.id.txtSanctionedAmount);
 
+            btnShare = view.findViewById(R.id.btnShare);
+            checkbox = view.findViewById(R.id.checkbox);
         }
     }
 
